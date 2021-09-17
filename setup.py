f08403a4a002a14e7f206305007d8b74c3cccbcb
Yury Sokov <ysokov@croc.ru>
2021-09-17 19:21:05 +0300

    version: bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 module_info = {
     "name": "ihashmap",
-    "version": "1.0.5",
+    "version": "1.0.6",
     "description": "Wrapper for hash map based storage systems",
     "author": "Yury Sokov aka. Yurzs",
     "author_email": "yurzs+ihashmap@icloud.com",
